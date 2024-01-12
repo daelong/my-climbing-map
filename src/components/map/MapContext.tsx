76f6bdcf96f37b1b5cb87b5eb6866936f1bae985
이대현(서비스개발그룹 REF팀) <dhlee2@autocrypt.io>
2024-01-12 16:56:00 +0900

    feat: openlayer 맵 세팅

---
+++ src/components/map/MapContext.tsx
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const MapContext = new createContext<any>({});
+export default MapContext;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// import Map from "../src/components/map/MapProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* <Map> */}
     <App />
-  </React.StrictMode>,
-)
+    {/* </Map> */}
+  </React.StrictMode>
+);
