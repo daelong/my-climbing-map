8a2d89b69ebe19c41643c171293595573f45fd6b
daelong <dleogus0569@naver.com>
2024-01-17 23:57:05 +0900

    feat: path 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: "127.0.0.1",
+    port: 8080,
+  },
 })
